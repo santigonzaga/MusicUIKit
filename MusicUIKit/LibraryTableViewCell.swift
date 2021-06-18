b3b5e5e86e8ab47de24880426705620b443c9719
Santiago <santiago.dgonzaga@hotmail.com>
2021-06-18 16:27:52 -0300

    library finalização

---
+++ MusicUIKit/LibraryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryTableViewCell.swift
+//  MusicUIKit
+//
+//  Created by Santiago del Castillo Gonzaga on 18/06/21.
+//
+
+import UIKit
+
+class LibraryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var libraryImageView: UIImageView!
+    
+    @IBOutlet weak var libraryTitleLabel: UILabel!
+    
+    @IBOutlet weak var librarySubtitleLabel: UILabel!
+}
--- MusicUIKit/ViewController.swift
+++ MusicUIKit/ViewController.swift
@@ -7,15 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableOfLibrary: UITableView!
-    
-    
-    
+    let musicService = try? MusicService()
+    var playlist: [MusicCollection] = []
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // ID: PlaylistCell
+        
+        tableOfLibrary.dataSource = self
+        playlist = musicService?.loadLibrary() ?? []
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playlist.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableOfLibrary.dequeueReusableCell(withIdentifier: "PlaylistCell", for: indexPath) as? LibraryTableViewCell else {
+            fatalError("Não foi possivel converter a celula para libraryCell")
+        }
+        
+        let music = playlist[indexPath.row]
+        cell.libraryImageView.image = musicService?.getCoverImage(forItemIded: music.id)
+        cell.libraryTitleLabel.text = music.title
+        cell.librarySubtitleLabel.text = music.type.rawValue.capitalized + " - " + music.mainPerson
+        
+        return cell
     }
 
 
