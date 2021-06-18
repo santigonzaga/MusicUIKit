6d9edf2601c237fd4686c0bce95cb3eb4fddc582
Heitor Kunrath <heitorkunrath@gmail.com>
2021-06-18 15:11:20 -0300

    library começo

--- MusicUIKit/ViewController.swift
+++ MusicUIKit/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableOfLibrary: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
