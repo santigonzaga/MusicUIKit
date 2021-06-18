ccc4f861ee5aaa11de82e6e57900205ba0189a03
Santiago <santiago.dgonzaga@hotmail.com>
2021-06-18 13:14:15 -0300

    Initial Commit

---
+++ MusicUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicUIKit
+//
+//  Created by Santiago del Castillo Gonzaga on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
