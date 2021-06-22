6724270f6a01953453dedef9f0056405a88be2ac
Santiago <santiago.dgonzaga@hotmail.com>
2021-06-22 16:10:38 -0300

    AlbumDetails table view

--- MusicUIKit/ModalInfoAlbum.swift
+++ MusicUIKit/ModalInfoAlbum.swift
@@ -7,26 +7,56 @@
 
 import UIKit
 
-class ModalInfoAlbum: UIViewController {
+class ModalInfoAlbum: UIViewController, UITableViewDataSource {
     
+    @IBOutlet weak var albumInfoTableVIew: UITableView!
     
     var playlist: MusicCollection?
+    let musicService = try? MusicService()
+    var section = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        albumInfoTableVIew.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if section == 2 {
+            guard let cell = albumInfoTableVIew.dequeueReusableCell(withIdentifier: "ArtistCell", for: indexPath) as? InfoArtistTableViewCell else {
+                fatalError("Não foi possivel converter a celula para ArtistCell")
+            }
+            
+            cell.artistNameLabel.text = playlist?.mainPerson
+            cell.textInfoLabel2.text = playlist?.albumArtistDescription
+            
+            return cell
+        } else {
+            guard let cell = albumInfoTableVIew.dequeueReusableCell(withIdentifier: "AlbumCell", for: indexPath) as? InfoAlbumTableViewCell else {
+                fatalError("Não foi possivel converter a celula para AlbumCell")
+            }
+
+            let date = playlist?.referenceDate
+            let format = DateFormatter()
+            format.dateFormat = "MMM ddd, yyyy"
+            let formattedDate = format.string(from: date!)
+            
+            cell.coverImage.image = musicService?.getCoverImage(forItemIded: playlist?.id ?? "")
+            cell.artistLabel.text = playlist?.mainPerson
+            cell.nameAlbumLabel.text = playlist?.title
+            cell.numberOfSongsLabel.text = String(playlist?.musics.count ?? 0) + " Songs"
+            cell.releasedLabel.text = "Released " + formattedDate
+            cell.textInfoLabel.text = playlist?.albumDescription
+            
+            section += 1
+            
+            return cell
+        }
+    }
 
 }
