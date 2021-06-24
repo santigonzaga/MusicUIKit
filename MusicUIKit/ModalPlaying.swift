d7a83e16e51e5287bd993b5fed5b849ef626efe0
Heitor Kunrath <heitorkunrath@gmail.com>
2021-06-24 17:59:17 -0300

    quase modal

---
+++ MusicUIKit/ModalPlaying.swift
@@ -0,0 +1,45 @@
+//
+//  ModalPlaying.swift
+//  MusicUIKit
+//
+//  Created by Heitor Feijó Kunrath on 24/06/21.
+//
+
+import UIKit
+
+class ModalPlaying: UIViewController {
+    
+    
+    
+    @IBOutlet weak var cover: UIImageView!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var musicName: UILabel!
+    @IBOutlet weak var artistName: UILabel!
+    @IBOutlet weak var musicSlider: UISlider!
+    var music : Music?
+    var musicService: MusicService?
+    @IBAction func likeDislike(_ sender: Any) {
+        guard let music = music, let musicService = musicService else {
+            return
+        }
+        var isFavorite = musicService.favoriteMusics.contains(music)
+        musicService.toggleFavorite(music: music, isFavorite: isFavorite)
+        isFavorite = !isFavorite
+        
+        favoriteButton.tintColor = isFavorite ? .systemRed : .black
+        favoriteButton.setImage(isFavorite ? UIImage(systemName: "heart.fill") : UIImage(systemName: "heart"), for: .normal)
+    }
+    @IBAction func dismissChevron(_ sender: Any) {
+        navigationController?.dismiss(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        musicSlider.setThumbImage(UIImage(systemName: "circle.fill"), for: .normal)
+        musicSlider.setThumbImage(UIImage(systemName: "circle.fill"), for: .highlighted)
+        
+    }
+    
+    
+    
+    
+}
